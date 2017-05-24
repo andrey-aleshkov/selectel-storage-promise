c42bfc815e72d79221798afc757869340aeb4b21
Andrey Aleshkov <a.aleshkov@rambler-co.ru>
2017-05-24 16:08:20 +0300

    before() and after() blocks

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const selectel = require('../index');
 const fs = require('fs');
 const targz = require('tar.gz');
 
+const containerName = 'mocha';
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 describe('Storage', function() {
@@ -24,54 +26,62 @@ describe('Containers', function() {
   });
 
   it('createContainer', async () => {
-    let response = await selectel.createContainer('mocha', 'private');
+    let response = await selectel.createContainer(containerName, 'private');
     expect(response.statusCode).to.equal(201);
   });
 
   it('infoContainer', async () => {
-    let response = await selectel.infoContainer('mocha');
+    let response = await selectel.infoContainer(containerName);
     expect(response.statusCode).to.equal(204);
   });
 
   it('editContainer', async () => {
-    let response = await selectel.editContainer('mocha', 'public');
+    let response = await selectel.editContainer(containerName, 'public');
     expect(response.statusCode).to.equal(202);
+  });
+
+  it('deleteContainer', async () => {
+    let response = await selectel.deleteContainer(containerName);
+    expect(response.statusCode).to.equal(204);
   });
 });
 
 describe('Files', function() {
+  before('create a container', async () => {
+    await selectel.createContainer(containerName, 'private');
+  });
+
   it('fetchFiles', async () => {
-    let response = await selectel.fetchFiles('mocha', {
+    let response = await selectel.fetchFiles(containerName, {
       format: 'json'
     });
     expect(response.statusCode).to.equal(200);
   });
 
   it('uploadFile', async () => {
-    let response = await selectel.uploadFile(__dirname + '/files/file.jpg', '/mocha/file.jpg');
+    let response = await selectel.uploadFile(__dirname + '/files/file.jpg', `/${containerName}/file.jpg`);
     expect(response.statusCode).to.equal(201);
   });
 
   it('extractArchive', async () => {
     let read = targz({}, { fromBase: true }).createReadStream(__dirname + '/files');
-    let response = await selectel.extractArchive(read, '/mocha', 'tar.gz');
+    let response = await selectel.extractArchive(read, `/${containerName}`, 'tar.gz');
     expect(response.statusCode).to.equal(201);
   }).timeout(10000);
 
   it('copyFile', async () => {
-    let response = await selectel.copyFile('/mocha/file.jpg', '/mocha/file-copy.jpg');
+    let response = await selectel.copyFile(`/${containerName}/file.jpg`, `/${containerName}/file-copy.jpg`);
     expect(response.statusCode).to.equal(201);
   });
 
   it('deleteFile', async () => {
-    let response = await selectel.deleteFile('/mocha/file.jpg');
+    let response = await selectel.deleteFile(`/${containerName}/file.jpg`);
     expect(response.statusCode).to.equal(204);
   });
-});
 
-//describe('Containers', function() {
-//  it('deleteContainer', async () => {
-//    let response = await selectel.deleteContainer('mocha');
-//    expect(response.statusCode).to.equal(204);
-//  });
-//});
+  after('delete the container', async () => {
+    await selectel.deleteFile(`/${containerName}/file-copy.jpg`);
+    await selectel.deleteFile(`/${containerName}/DS_Store`);
+    await selectel.deleteContainer(containerName);
+  });
+});
