7c5bfe195acb089806b32bf80697a3000c6d79b2
Andrey Aleshkov <a.aleshkov@rambler-co.ru>
2017-05-29 18:40:32 +0300

    chai-as-promised

--- test/unit.tests.js
+++ test/unit.tests.js
@@ -1,47 +1,146 @@
-const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const Selectel = require('../selectel');
 const sinon = require('sinon');
 
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const validCredentials = {
+  login: 'validLogin',
+  pass: 'validPass'
+};
+const invalidCredentials = {
+  login: 'invalidLogin',
+  pass: 'invalidPass'
+};
+const containerName = 'tests';
+const usedContainerName = 'tests-used';
 const requestPromise = {
   defaults: function() {
-    return function() {
+    return function(params) {
+      let url = params.url;
+      let login = params.headers['X-Auth-User'];
+      let pass = params.headers['X-Auth-Key'];
+
+      // console.log('login = ', login);
+      // console.log('pass = ', pass);
+
       return new Promise((resolve, reject) => {
         // response
-        // reject({});
-        resolve({
-          headers: {
-            'x-expire-auth-token': 1,
-            'x-storage-url': '',
-            'x-auth-token': ''
-          },
-          statusCode: 204
-        });
+        if (login === validCredentials.login && pass === validCredentials.pass) {
+          resolve({
+            headers: {
+              'x-expire-auth-token': 1,
+              'x-storage-url': '',
+              'x-auth-token': ''
+            },
+            statusCode: 204
+          });
+        } else {
+          reject({
+            statusCode: 403
+          });
+        }
       });
     };
   }
 };
-
 const request = sinon.mock();
 const mock = sinon.mock(requestPromise);
 const selectel = new Selectel(request, requestPromise);
 
-const validCredentials = {
-  login: '',
-  pass: ''
-};
-const invalidCredentials = {
-  login: '',
-  pass: ''
-};
+describe('Get the authentication token', function() {
+  it('with invalid credentials', async () => {
+    let response = await selectel.auth(invalidCredentials.login, invalidCredentials.pass);
+    expect(response).to.be.rejected.and.notify(done); // .equal(403)
+    // response.should.be.rejected;
+  });
 
-describe('Storage', function() {
-  it('auth with valid credentials', async () => {
+  it('with valid credentials', async () => {
     let response = await selectel.auth(validCredentials.login, validCredentials.pass);
     expect(response.statusCode).to.equal(204);
   });
+});
 
-  it('auth with invalid credentials', async () => {
-    let response = await selectel.auth(invalidCredentials.login, invalidCredentials.pass);
-    expect(response.statusCode).to.equal(403);
+describe('Get information', function() {
+  it('info', async () => {
+    let response = await selectel.info();
+    expect(response.statusCode).to.equal(204);
+  });
+});
+
+describe('Return the list of available containers', function() {
+  it('in default format', async () => {
+    let response = await selectel.fetchContainers();
+    expect(response.statusCode).to.equal(200);
+  });
+
+  it('in json format', async () => {
+    let response = await selectel.fetchContainers('json');
+    expect(response.statusCode).to.equal(200);
+  });
+
+  it('in xml format', async () => {
+    let response = await selectel.fetchContainers('xml');
+    expect(response.statusCode).to.equal(200);
+  });
+});
+
+describe('Create a new container', function() {
+  it('with name', async () => {
+    let response = await selectel.createContainer(containerName, 'private');
+    expect(response.statusCode).to.equal(201);
+  });
+
+  it('with already used name', async () => {
+    let response = await selectel.createContainer(usedContainerName, 'private');
+    expect(response.statusCode).to.equal(201);
+  });
+
+  it('without name', async () => {
+    let response = await selectel.createContainer();
+    expect(response.statusCode).to.equal(201);
+  });
+
+  it('without type', async () => {
+    let response = await selectel.createContainer(containerName);
+    expect(response.statusCode).to.equal(201);
+  });
+
+  it('as public', async () => {
+    let response = await selectel.createContainer(containerName, 'public');
+    expect(response.statusCode).to.equal(201);
+  });
+
+  it('as private', async () => {
+    let response = await selectel.createContainer(containerName, 'private');
+    expect(response.statusCode).to.equal(201);
+  });
+
+  it('as gallery', async () => {
+    let response = await selectel.createContainer(containerName, 'gallery');
+    expect(response.statusCode).to.equal(201);
+  });
+});
+
+describe('infoContainer', function() {
+  it('infoContainer', async () => {
+    let response = await selectel.infoContainer(containerName);
+    expect(response.statusCode).to.equal(204);
+  });
+});
+
+describe('createContainer', function() {
+  it('editContainer', async () => {
+    let response = await selectel.editContainer(containerName, 'public');
+    expect(response.statusCode).to.equal(202);
+  });
+});
+
+describe('createContainer', function() {
+  it('deleteContainer', async () => {
+    let response = await selectel.deleteContainer(containerName);
+    expect(response.statusCode).to.equal(204);
   });
 });
